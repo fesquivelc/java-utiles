5360d7900d9d8135f701ac529e586d4fbcd4cf97
fesquivelc <fesquivelc@localhost>
2015-01-04 17:45:26 -0500

    AGREGADO FECHA UTIL

--- src/main/java/com/personal/utiles/FechaUtil.java
+++ src/main/java/com/personal/utiles/FechaUtil.java
@@ -22,54 +22,69 @@ public class FechaUtil {
         if (fecha1.compareTo(fecha2) < 0) {
             return -1;
         } else if (fecha1.compareTo(fecha2) == 0) {
-            LOG.log(Level.INFO, "hora1 {0} vs hora2 {1} : {2}", new String[]{hora1.toString(), hora2.toString(), hora1.compareTo(hora2) + ""});
             return hora1.compareTo(hora2);
         } else {
             return 1;
         }
     }
-    
-    public static int[] milisToTime(long milis){
+
+    public static int[] milisToTime(long milis) {
         int[] hora = new int[3];
-        
+
         hora[0] = milisAHoras(milis);
-        
+
         milis = milis - horasAMilis(hora[0]);
-        
+
         hora[1] = milisAMinutos(milis);
-        
+
         milis = milis - minutosAMilis(hora[1]);
-        
+
         hora[2] = milisASegundos(milis);
-        
+
         return hora;
     }
-    
+
     public static int ultimoDiaMes(int mes, int anio) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(anio, mes - 1, 1);
         return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
     }
-    
-    public static int horasAMilis(int horas){
+
+    public static int horasAMilis(int horas) {
         return horas * 60 * 60 * 1000;
     }
-    
-    public static int minutosAMilis(int minutos){
+
+    public static int minutosAMilis(int minutos) {
         return minutos * 60 * 1000;
     }
-    
-    public static int milisAHoras(long milis){
+
+    public static int milisAHoras(long milis) {
         return (int) milis / (60 * 60 * 1000);
     }
-    
-    public static int milisAMinutos(long milis){
+
+    public static int milisAMinutos(long milis) {
         return (int) milis / (60 * 1000);
     }
-    
-    public static int milisASegundos(long milis){
+
+    public static int milisASegundos(long milis) {
         return (int) milis / 1000;
     }
 
+    public static Date soloHora(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(1970, 0, 1);
+        return cal.getTime();
+    }
+
+    public static Date soloFecha(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
 
 }
