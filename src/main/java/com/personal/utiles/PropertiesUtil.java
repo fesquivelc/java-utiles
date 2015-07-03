ed363ad5f050d86c91726f22dfb64b19fd454431
fesquivelc <fesquivelc@gmail.com>
2015-07-03 17:34:25 -0500

    utiles papi

--- src/main/java/com/personal/utiles/PropertiesUtil.java
+++ src/main/java/com/personal/utiles/PropertiesUtil.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 /**
